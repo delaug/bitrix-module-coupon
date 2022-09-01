b42373f3976517cde47136037dd00fc20a960c39
Sergei <delaug@bk.ru>
2022-09-01 18:02:02 +0300

    Controller

---
+++ .settings.php
@@ -0,0 +1,10 @@
+<?php
+//modules/vendor.example/.settings.php
+return [
+    'controllers' => [
+        'value' => [
+            'defaultNamespace' => '\\Bitrix\\Coupon\\Controller',
+        ],
+        'readonly' => true,
+    ]
+];
